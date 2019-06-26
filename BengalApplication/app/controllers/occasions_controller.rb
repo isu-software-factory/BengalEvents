6818f0be221563208b0f0bb3cf555a7ccebaa25f
AdharBhattarai <bhatadha@isu.edu>
2019-06-25 18:46:38 -0600

    Add CRUD on Occasion

--- BengalApplication/app/controllers/occasions_controller.rb
+++ BengalApplication/app/controllers/occasions_controller.rb
@@ -3,13 +3,17 @@ class OccasionsController < ApplicationController
     @occasions = Occasion.all
   end
 
+  def new
+    @occasion = Occasion.new
+  end
+
   def create
     occasion = Occasion.new(occasion_params)
     if occasion.save
-      redirect_to "/occasions"
+      redirect_to occasions_path
     else
       flash[:errors] = occasion.errors.full_messages
-      redirect_to "/occasions/new"
+      redirect_back(fallback_location: new_occasion_path)
     end
   end
 
@@ -21,7 +25,24 @@ class OccasionsController < ApplicationController
     @occasion = Occasion.find(params[:id])
   end
 
+  def update
+    occasion = Occasion.find(params[:id])
+    if occasion.update(occasion_params)
+      redirect_to occasions_path
+    else
+      flash[:errors] = occasion.errors.full_messages
+      redirect_back(fallback_location: edit_occasion_path(occasion.id))
+    end
+  end
+
+  def destroy
+    occasion = Occasion.find(params[:id])
+    occasion.destroy
+    redirect_to occasions_path
+  end
+
   private
+
   def occasion_params
     params.require(:occasion).permit(:name, :end_date, :start_date)
   end
--- BengalApplication/config/routes.rb
+++ BengalApplication/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :teachers do
     resources :students
   end
-  resources :occasions, only: [:index, :new, :create, :show, :edit, :update]
+  resources :occasions
   root 'homeroutes#route'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
