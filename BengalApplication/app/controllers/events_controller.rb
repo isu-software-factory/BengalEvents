7f6cbee3b28099297adf6388bafcf452bd4b209d
AdharBhattarai <bhatadha@isu.edu>
2019-06-28 15:03:32 -0600

    Add Create and Delete on Event

--- BengalApplication/app/controllers/events_controller.rb
+++ BengalApplication/app/controllers/events_controller.rb
@@ -1,14 +1,24 @@
 class EventsController < ApplicationController
+  before_action :find_occasion
+  before_action :find_event, only: [:destroy]
 
   def create
-    @occasion = Occasion.find(params[:occasion_id])
-    @event = @occasion.create(param[:comment].permit(:name, :location,:description, :isMakeAhead))
+    @event = @occasion.events.create(params[:event].permit(:name, :location, :description))
     redirect_to occasion_path(@occasion)
   end
 
   def destroy
+      @event.destroy
+      redirect_to occasion_path(@occasion)
+  end
+
+  private
+
+  def find_occasion
     @occasion = Occasion.find(params[:occasion_id])
-    @event =@occasion.events.find(params[:id])
-    @event.destroy
+  end
+
+  def find_event
+    @event = @occasion.events.find(params[:id])
   end
 end
--- BengalApplication/app/models/event.rb
+++ BengalApplication/app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :occasion
-  has_many :sessions, dependent: :destroy
-  validates :name, presence: true
-  validates :location, presence: true
-  validates :description, presence: true
-  validates :isMakeAhead, presence: true
+  has_many :periods, dependent: :destroy
+  # validates :name, presence: true
+  # validates :location, presence: true
+  # validates :description, presence: true
+  # validates :isMakeAhead, presence: true
 end
