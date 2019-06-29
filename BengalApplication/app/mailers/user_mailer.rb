ded146c9e1cdf874d645c9fd584d209115a3bb88
Daniel Cano <canojuan@isu.edu>
2019-06-29 13:19:27 -0600

    finished mailer, need gmail password

---
+++ BengalApplication/app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  # UserMailer.welcome_email(@user).deliver_now
+  def login_email(user, link)
+    @user = user
+    @link = link
+    mail(to: @user.email, subject: "Welcome to BengalEvents")
+  end
+
+end
