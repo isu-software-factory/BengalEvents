6a549d6e8e08bea132882e67a100a02785901d94
AdharBhattarai <bhatadha@isu.edu>
2019-07-03 18:38:08 -0600

    Update controller

--- BengalApplication/config/routes.rb
+++ BengalApplication/config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :occasions do
     resources :events
   end
-  resources :users, only: [:index, :show, :update, :destroy]
 
   root 'homeroutes#routes'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
