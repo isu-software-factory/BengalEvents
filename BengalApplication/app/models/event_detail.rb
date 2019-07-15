2d36a9451ec0f622f789f52a8e7746fac6e5b7bd
AdharBhattarai <bhatadha@isu.edu>
2019-07-15 16:43:27 -0600

    Add unqiue validations on Registration

--- BengalApplication/app/models/event_detail.rb
+++ BengalApplication/app/models/event_detail.rb
@@ -2,4 +2,6 @@ class EventDetail < ApplicationRecord
   belongs_to :event
   has_many :registrations
   has_many :students , through: :registrations
+  # validates :start_time, uniqueness: true
+  # validates :end_time, uniqueness: true
 end
--- BengalApplication/app/models/registration.rb
+++ BengalApplication/app/models/registration.rb
@@ -1,4 +1,7 @@
 class Registration < ApplicationRecord
   belongs_to :student
   belongs_to :event_detail
+  validates :student, uniqueness: true
+  validates :event_detail, uniqueness: true
+
 end
