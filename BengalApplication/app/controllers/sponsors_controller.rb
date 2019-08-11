6bc9747f7e9a4c5aac79b72825db80eae9a587df
AdharBhattarai <bhatadha@isu.edu>
2019-08-11 00:26:17 -0600

    Update on home routes view

--- BengalApplication/app/controllers/sponsors_controller.rb
+++ BengalApplication/app/controllers/sponsors_controller.rb
@@ -1,39 +1,40 @@
 class SponsorsController < ApplicationController
   before_action :authenticate_user!, except: [:new, :create]
 
-    def new
-      @sponsor = Sponsor.new
-      @sponsor.build_user
-    end
+  def new
+    @sponsor = Sponsor.new
+    @sponsor.build_user
+    authorize @sponsor
+  end
 
-    def create
-      @sponsor = Sponsor.new(sponsor_params)
-      if @sponsor.save
-        sign_in @sponsor.user
-        redirect_to @sponsor
-      else
-        render :new
-      end
-    end
-
-    def show
-      @sponsor = Sponsor.find(params[:id])
-      @occasions = Occasion.all
-    end
-
-    def edit
-    end
-
-    def update
-    end
-
-    def destroy
-    end
-
-    private
-
-    def sponsor_params
-      params.require(:sponsor).permit(:name, user_attributes: [:id, :email, :password])
+  def create
+    @sponsor = Sponsor.new(sponsor_params)
+    if @sponsor.save
+      sign_in @sponsor.user
+      redirect_to @sponsor
+    else
+      render :new
     end
   end
+
+  def show
+    @sponsor = Sponsor.find(params[:id])
+    @occasions = Occasion.all
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def sponsor_params
+    params.require(:sponsor).permit(:name, user_attributes: [:id, :email, :password])
+  end
+end
 
