70b148d5a66478c959b897081b85afb289896d16
SilverTaurus <dcano799@gmail.com>
2019-09-05 18:52:37 -0600

    added styles to team cards in student

--- BengalApplication/app/controllers/students_controller.rb
+++ BengalApplication/app/controllers/students_controller.rb
@@ -7,17 +7,25 @@ class StudentsController < ApplicationController
      @students = Student.where("teacher_id = ?", params[:teacher_id])
    end
 
+  # shows the schedule of a student
   def schedule
-    # shows the schedule of a student
     @student = Student.find(params[:id])
+
+    # add breadcrumbs
     add_breadcrumb "Home", current_user.meta
     add_breadcrumb "#{@student.name}'s Schedule", controller: "students", action: "schedule", id: @student.id
   end
 
+  # shows student main page
   def show
     @student = Student.find(params[:id])
     authorize @student
+
+    # add breadcrumbs
     add_breadcrumb "Home", @student
+
+    # get the number of teams per row
+    # for the student page
     @teams = 1
     @count = 0
     @increase = 1
@@ -28,65 +36,74 @@ class StudentsController < ApplicationController
       end
       @increase += 1
     end
+
+    # list of card styles
+    @random_card = ["bg-primary", "bg-success", "bg-info", "bg-light", "bg-dark"]
   end
 
+  # shows the student new page
   def new
     @student = Student.new
     @student.build_user
     authorize @student
   end
 
+  # creates a new student
   def create
     names = student_names
     emails = student_emails
     authorize Student
 
+    # for each student in array, create a student
     for each in 0..(names.count - 1) do
-      # if an error occurs
-
       success = create_students(names[each], emails[each])
-
     end
     redirect_to teacher_path(@teacher.id)
+
   end
 
   private
-
   def prepare_teacher
     @teacher = Teacher.find(current_user.meta.id)
   end
 
+  # returns all the names from params into an array
   def student_names
     names = []
+    # add each name into names array
     for num in 1..@teacher.student_count do
       names << params["name" + num.to_s]
     end
     names
   end
 
+  # returns emails of all students as an array
   def student_emails
     emails = []
+    # adds every email from params to emails array
     for num in 1..@teacher.student_count do
       emails << params["email" + num.to_s]
     end
     emails
   end
 
+  # creates a student
   def create_students(name1, email1)
     @student = Student.new(name: name1, user_attributes: {email: email1}, participant_attributes: {})
     @teacher.students << @student
+
     # create password
     random_password = ('0'..'z').to_a.shuffle.first(8).join
     @student.user.password = random_password
     @student.user.password_confirmation = random_password
 
+    # send an email to student if student saves
     if @student.save
       UserMailer.login_email(@student, @student.user, random_password).deliver_now
       true
     else
-      asasda
       false
     end
-
   end
+
 end
--- BengalApplication/app/controllers/teachers_controller.rb
+++ BengalApplication/app/controllers/teachers_controller.rb
@@ -1,16 +1,20 @@
 class TeachersController < ApplicationController
   before_action :authenticate_user!, except: [:new, :create]
 
-
+  # shows teacher new page
   def new
     @teacher = Teacher.new
     @teacher.build_user
   end
 
+  # creates a new teacher
   def create
+    # create teacher and participant
     @teacher = Teacher.new(teacher_params)
     Participant.create(member: @teacher)
+
     if @teacher.save
+      # sign in teacher and redirect to students new page
       sign_in @teacher.user
       redirect_to "/students/new"
     else
@@ -18,6 +22,7 @@ class TeachersController < ApplicationController
     end
   end
 
+  # shows teacher main page
   def show
     @teacher = Teacher.find(params[:id])
     # respond_to do |format|
@@ -32,30 +37,38 @@ class TeachersController < ApplicationController
     add_breadcrumb "Home", @teacher
   end
 
+  # shows class registrations page
   def class_registrations
     # display every student registrations
     @teacher = Teacher.find(params[:id])
     @students = @teacher.students
+
+    # add breadcrumbs
     add_breadcrumb "Home", current_user.meta
     add_breadcrumb "Class Registrations", controller: "teachers", action: "class_registrations", id: @teacher.id
   end
 
 
-  def edit
-    @teacher = Teacher.find(params[:id])
-    authorize @teacher
-    add_breadcrumb "Account Settings", edit_teacher_path(@teacher)
-  end
+  # def edit
+  #   @teacher = Teacher.find(params[:id])
+  #   authorize @teacher
+  #
+  #   # add breadcrumbs
+  #   add_breadcrumb "Account Settings", edit_teacher_path(@teacher)
+  # end
 
-  def update
-    @teacher = Teacher.find(params[:id])
-    authorize @teacher
-    if @teacher.update_attributes(teacher_params)
-      redirect_to @teacher, :notice => "Successfully updated"
-    else
-      render 'edit'
-    end
-  end
+  # # shows edit page
+  # def update
+  #   @teacher = Teacher.find(params[:id])
+  #   authorize @teacher
+  #
+  #   # update teacher and redirect to teacher page
+  #   if @teacher.update_attributes(teacher_params)
+  #     redirect_to @teacher, :notice => "Successfully updated"
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 
   private
 
@@ -63,6 +76,7 @@ class TeachersController < ApplicationController
     params.require(:teacher).permit(:school, :name, :student_count, :chaperone_count, user_attributes: [:id, :email, :password, :password_confirmation])
   end
 
+  # sort students by name
   def organize_students(students_list)
     students_list.sort_by{|s| s.name}
   end
