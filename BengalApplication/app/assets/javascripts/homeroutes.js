d0d2e5d47519b71f86d743ece6336c34b42219eb
SilverTaurus1 <dcano799@gmail.com>
2020-02-12 20:43:49 -0700

    updated UI for registering

--- BengalApplication/app/assets/javascripts/homeroutes.js
+++ BengalApplication/app/assets/javascripts/homeroutes.js
@@ -21,9 +21,24 @@ $(document).on('ready page:load turbolinks:load', function() {
                     alert("false");
                 }
             }
-
         })
     });
 
+    $(".accordion").hide();
+
+    $(".accordion").parent().parent().prev().click(function(){
+        const accordion = $(this).next().children().first().children().first();
+        down_icon = "glyphicon glyphicon-menu-down";
+        up_icon = "glyphicon glyphicon-menu-up";
+        if (accordion.hasClass("Down")){
+            accordion.slideUp();
+            accordion.removeClass("Down");
+            $(this).children().last().children().first().removeClass(up_icon).addClass(down_icon);
+        }else {
+            accordion.slideDown();
+            accordion.addClass("Down");
+            $(this).children().last().children().first().removeClass(down_icon).addClass(up_icon);
+        }
+    });
 
 });
