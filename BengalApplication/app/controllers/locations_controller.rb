e21915d6d9da00845d19a3f0b6f9675d9b6c65c1
SilverTaurus1 <dcano799@gmail.com>
2020-06-27 16:59:14 -0600

    Updated Manage Locations Page

--- BengalApplication/app/controllers/locations_controller.rb
+++ BengalApplication/app/controllers/locations_controller.rb
@@ -27,65 +27,43 @@ class LocationsController < ApplicationController
     end
   end
 
+  def new
+    @location = Location.new
+    @action = "locations/create"
+    @remote = true
+  end
 
-  # def index
-  #   @location = Location.all
-  # end
-  #
-  # def new
-  #   @occasion = Occasion.find(params[:occasion_id])
-  #   @location = @occasion.locations.build
-  #   @time_slot = @location.time_slots.build
-  #   authorize @location
-  #
-  #   add_breadcrumb "Home", current_user.meta
-  #   add_breadcrumb "#{@occasion.name}", @occasion
-  #   add_breadcrumb "New Location", new_occasion_location_path(@occasion)
-  # end
-  #
-  # def create
-  #   @occasion = Occasion.find(params[:occasion_id])
-  #   @location = @occasion.locations.build(location_params)
-  #   # pry
-  #   authorize @location
-  #   if @location.save
-  #     redirect_to event_path(@occasion), notice: 'Successfully created Location.'
-  #   else
-  #     flash[:errors] = @location.errors.full_messages
-  #     redirect_back(fallback_location: new_occasion_location_path)
-  #   end
-  # end
-  #
-  # def edit
-  #   @occasion = Occasion.find(params[:occasion_id])
-  #   @location = Location.find(params[:id])
-  # end
-  #
-  # def update
-  #   @occasion = Occasion.find(params[:occasion_id])
-  #   @location = Location.find(params[:id])
-  #   if @location.update(location_params)
-  #     redirect_to event_path(@occasion), notice: 'Successfully updated Location.'
-  #   else
-  #     flash[:errors] = @location.errors.full_messages
-  #     redirect_back(fallback_location: edit_occasion_activity_path)
-  #   end
-  # end
-  #
-  # def show
-  #   @occasion = Occasion.find(params[:occasion_id])
-  #   @location = Location.find(params[:id])
-  #   authorize @location
-  # end
-  #
-  # def destroy
-  #   @occasion = Occasion.find(params[:occasion_id])
-  #   location = Location.find(params[:id])
-  #   authorize location
-  #   location.destroy
-  #   redirect_to event_path(@occasion), notice: 'Successfully deleted Location.'
-  # end
+  def create
+    @location = Location.new(location_name: params['location'], address: params['address'])
+    respond_to do |format|
+      if @location.save
+        format.js
+        format.html { redirect_to manage_locations_path, notice: "Successfully Created New Location." }
+      else
+        flash[:error] = @location.errors.full_message
+        redirect_to manage_location_path
+      end
+    end
+  end
 
-  private
+  def edit
+    @location = Location.find(params[:id])
+    @action = "locations/update"
+    @remote = true
+  end
 
-end
+  def update
+    @location = Location.find(params[:location_id])
+    respond_to do |format|
+      if @location.update(location_name: params['location'], address: params['address'])
+        format.html { redirect_to manage_locations_path }
+      else
+        format.html {
+          flash[:error] = @location.errors.full_message
+          redirect_to manage_location_path
+        }
+      end
+      end
+    end
+
+  end
