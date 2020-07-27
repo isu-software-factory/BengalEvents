2ffb2891e572aed13600ea556f6fcc0eb40f280c
SilverTaurus1 <dcano799@gmail.com>
2020-07-26 21:12:40 -0600

    updated load test

--- BengalApplication/spec/features/load_tests_spec.rb
+++ BengalApplication/spec/features/load_tests_spec.rb
@@ -12,16 +12,12 @@ RSpec.feature "LoadTests", type: :feature do
       stuff = []
       simple_test = GasLoadTester::Test.new({client: 100, time: 10})
       simple_test.run(output: true, file_name: 'load_results.html') do
-        page = Nokogiri::HTML(RestClient.get('localhost:3000/homeroutes/home'))
-        token = page.css('input[name="authenticity_token"]')
-        token2 = token[0]['value']
-
-        response = RestClient.post('localhost:3000/users/sign_in', authenticity_token: token2, users: {login: 'dil@gmail.com', password: 'password'}, commit: 'Sign in')
+        response = RestClient.get('localhost:3000/homeroutes/home')
         stuff << response
       end
 
       stuff.each do |s|
-        expect(s.body.include?('Bil')).to eq(true)
+        expect(s.body.include?('Bengal Stem Day')).to eq(true)
         puts s
       end
 
